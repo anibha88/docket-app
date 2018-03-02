64a1aa31dc92a94b30e8dc8e02f6265026676d73
Nithin Bharadhwaj <anibha88@gmail.com>
2018-03-02 17:16:57 +0530

    exploring jsx

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,6 +1,24 @@
 console.log("Hari Om!");
 // JSX - JavaScript XML
-var template = <h1> Hari Sarvothama Vaayu Jeevothama </h1>;
+var template = (
+    <div>
+        <h1> Hari Sarvothama Vaayu Jeevothama </h1> 
+            <p>It is further explained that Maha-Vishnu, who appears in the Karana Ocean (also called the Viraja River, which is the border between the spiritual and material world), becomes the resting place of the jiva-shakti. This is the collective energy of the individual spirit souls, the jivatmas. “There is one marginal potency, known as the jiva. Maha-Vishnu is the shelter of all jivas.” It is this Shri Hari who is the original source of all living entities because they are expansions of His marginal potency. Some become conditioned by the material energy while others are under the protection of the spiritual nature.
+            </p> 
+            <p>Lord Maha-Vishnu is the source of thousands of avataras in His thousands and thousands of subjective portions. He is the creator of countless individual souls. He is also known by the name of Narayana, meaning the shelter of all the individual jiva souls. 
+            </p> 
+            <ol>
+                <li> Swaroopa yogyatha of Vayu is very high compared to other Jivas. He is known as “Riju” </li>
+                <li> No one equals Vayu in qualities like Bhakthi, Gnana, Vairaagya  (Among jivas)</li>
+                <li> There is no kali prevesha for vayu deva. (This is same for all Parashukla thrayaru**)</li>
+                <li>  He does not do any activities against the will of Sri Hari</li>
+                <li> He is the first among Ekaanta Bhaktas. (Bhaktas donot ask anything from Lord for their seva is called Ekaanta Bhaktas. They don’t even ask Moksha from God.) </li>
+                <li> When all the Tathvaabhimani devatas in our body leaves a person is able to survive. But when Vayu devaru leaves the human body then the person cannot survive. The mere existence of all Jivas is dependent on Vayu devaru.</li>
+                <li> Even though he is having Linga deha (bondage) it does not have any effect on Vayu devaru.</li>
+            </ol>
+    </div>
+);
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template,appRoot);
